95f0232ae452205d0e270963ce9127b85594896a
Goby56 <karl.rosengren.kr@gmail.com>
2022-01-18 15:11:51 +0100

    Migrated to 1.17.1
    Updated license to GNU GPLv3
    Added homepage and sources

--- src/main/java/com/goby56/strongholdfinder/mixin/StrongholdFinderMixin.java
+++ src/main/java/com/goby56/strongholdfinder/mixin/StrongholdFinderMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class StrongholdFinderMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		StrongholdFinder.LOGGER.info("This line is printed by an example mod mixin!");
+		StrongholdFinder.LOGGER.info("This line is printed by the stronghold-finder mod mixin!");
 	}
 }
