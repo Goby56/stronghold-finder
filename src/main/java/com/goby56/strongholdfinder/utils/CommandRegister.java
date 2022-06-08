3fbad9504f9ea7eab3297441e8ec0e567f1902ce
Goby56 <60710855+Goby56@users.noreply.github.com>
2022-06-08 17:50:45 +0200

    Updated to 1.19

--- src/main/java/com/goby56/strongholdfinder/utils/CommandRegister.java
+++ src/main/java/com/goby56/strongholdfinder/utils/CommandRegister.java
@@ -1,14 +1,17 @@
 package com.goby56.strongholdfinder.utils;
 
-import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 
 import static com.goby56.strongholdfinder.commands.LogPosition.logPos;
 
 public class CommandRegister {
     public static void registerCommand() {
-        ClientCommandManager.DISPATCHER.register(ClientCommandManager.literal("logpos").executes(context -> {
-            logPos(context);
-            return 1;
+        ClientCommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess) -> {
+            dispatcher.register(ClientCommandManager.literal("logpos").executes((context -> {
+                logPos(context);
+                return 1;
+            })));
         }));
     }
 }
