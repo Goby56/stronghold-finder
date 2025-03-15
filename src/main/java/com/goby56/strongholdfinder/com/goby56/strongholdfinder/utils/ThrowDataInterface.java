14ecdae4e6eedfbe4a00df05380b35dede978ce6
Goby56 <karl.rosengren.kr@gmail.com>
2025-03-15 17:27:31 +0100

    1.21.4

--- src/main/java/com/goby56/strongholdfinder/com/goby56/strongholdfinder/utils/ThrowDataInterface.java
+++
@@ -1,7 +0,0 @@
-package com.goby56.strongholdfinder.com.goby56.strongholdfinder.utils;
-
-import net.minecraft.nbt.NbtCompound;
-
-public interface ThrowDataInterface {
-    NbtCompound getThrowData();
-}
